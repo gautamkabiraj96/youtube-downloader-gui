5252450da6a37010b86c5263247a9fc78c2fc032
Gautam Kabiraj <gautamkabiraj@gmail.com>
2023-07-24 02:19:14 +0530

    GUI updated

--- video_downloader_gui.py
+++ video_downloader_gui.py
@@ -1,10 +1,22 @@
 import os
 import tkinter as tk
-from tkinter import filedialog
+from tkinter import filedialog, messagebox
 from tkinter.ttk import Combobox
 import traceback
 import youtube_dl
 import ffmpeg
+
+
+def show_about_info():
+    about_message = "Youtube Downloader v2023.07.24\n Developed by Gautam Kabiraj"
+    messagebox.showinfo("About", about_message)
+
+
+def download_progress_hook(d):
+    if d['status'] == 'downloading':
+        output_label.config(text="Downloading...")
+        root.update()
+
 
 def download_video():
     url = url_entry.get()
@@ -16,7 +28,8 @@ def download_video():
 
     ydl_opts = {
         'format': quality_filter,
-        'outtmpl': os.path.join(download_path, '%(title)s.%(ext)s')
+        'outtmpl': os.path.join(download_path, '%(title)s.%(ext)s'),
+        'progress_hooks': [download_progress_hook],
     }
 
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
@@ -45,32 +58,63 @@ def merge_audio_video():
 
 # GUI setup
 root = tk.Tk()
-root.title("YouTube Video Downloader & Merger")
+root.title("YouTube Video Downloader")
 
-url_label = tk.Label(root, text="YouTube URL:")
-url_label.pack()
+# Set the background color
+root.configure(bg='#f0f0f0')
 
-url_entry = tk.Entry(root, width=50)
-url_entry.pack()
+# Set the font
+font = ('Arial', 12)
+
+# Create a frame for the logo
+logo_frame = tk.Frame(root, bg='#f0f0f0')
+logo_frame.pack(pady=10)
+
+# Replace 'your_logo.png' with the path to your logo image
+logo_img = tk.PhotoImage(file='images/download.png')
+logo_label = tk.Label(logo_frame, image=logo_img, bg='#f0f0f0')
+logo_label.pack()
+
+# Main frame
+main_frame = tk.Frame(root, bg='#f0f0f0')
+main_frame.pack(padx=20, pady=10, anchor='center')
+
+url_label = tk.Label(main_frame, text="YouTube URL:", font=font, bg='#f0f0f0')
+url_label.grid(row=0, column=0, padx=5, pady=5, sticky='w')
+
+url_entry = tk.Entry(main_frame, width=50, font=font)
+url_entry.grid(row=0, column=1, padx=5, pady=5)
 
 quality_var = tk.StringVar()
-quality_var.set('1080p')  # Default quality selection
-quality_label = tk.Label(root, text="Video Quality:")
-quality_label.pack()
+quality_var.set('1080')  # Default quality selection
+quality_label = tk.Label(
+    main_frame, text="Video Quality:", font=font, bg='#f0f0f0')
+quality_label.grid(row=1, column=0, padx=5, pady=5, sticky='w')
 
-quality_combobox = Combobox(
-    root, textvariable=quality_var, values=['720p', '1080p'])
-quality_combobox.pack()
+quality_combobox = Combobox(main_frame, textvariable=quality_var, values=[
+                            '360', '480', '720', '1080', '1440'], font=font)
+quality_combobox.grid(row=1, column=1, padx=5, pady=5)
 
-download_button = tk.Button(
-    root, text="Download Video", command=download_video)
-download_button.pack()
+download_button = tk.Button(main_frame, text="Download Video",
+                            font=font, bg='#ff0000', fg='#ffffff', command=download_video)
+download_button.grid(row=2, column=0, columnspan=2, padx=5, pady=10)
+
+output_label = tk.Label(main_frame, text="", font=font, bg='#f0f0f0')
+output_label.grid(row=3, column=0, columnspan=2, padx=5, pady=5)
+
+# Create a menu bar
+menu_bar = tk.Menu(root)
+root.config(menu=menu_bar)
+
+# Add "File" menu to the menu bar
+file_menu = tk.Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label="File", menu=file_menu)
+
+# Add "About" option under the "File" menu
+file_menu.add_command(label="About", command=show_about_info)
 
 # merge_button = tk.Button(
 #     root, text="Merge Video and Audio", command=merge_audio_video)
 # merge_button.pack()
-
-output_label = tk.Label(root, text="")
-output_label.pack()
 
 root.mainloop()
